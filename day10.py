4723f169122f06fb43e1970dd7b37b165c81c980
Sakshamtyagi07 <sakshamtyagi8791@gmail.com>
2026-09-23 23:07:18 +0530

    done for day10

---
+++ day10.py
@@ -0,0 +1,71 @@
+## STAGE 2 ##
+
+
+# Question 1:- basic pratice
+# Then print 
+# First element
+# Last element
+# Element at index 2
+# Length of the list
+
+
+# arr = [10,20,30,40,50]
+# print(arr[0])
+# print(arr[-1])
+# print(arr[2])
+# print(len(arr))
+
+
+# Q2 — Traversal
+# arr = [10,20,30,40,50]
+# for i in range(len(arr)):
+#     print(arr[i])
+
+
+# Q3 — Direct Traversal
+# arr = [10,20,30,40,50]
+# for x in arr:
+#     print(x)
+
+
+# Q4 — Find the Sum of Array Elements
+# arr = [10,20,30,40,50]
+# sum = 0
+# for i in range(len(arr)):
+#     sum = sum + arr[i]
+# print(sum)
+
+
+# Q5 — Find the Largest Element
+# arr = [10,20,60,40,50]
+# largest = 0
+# for i in range(len(arr)):
+#     if arr[i] > largest:
+#         largest  = arr[i]
+# print(largest)
+
+
+# Q6 — Find the Smallest Element
+# arr = [10,25,60,4,50]
+# smallest = float("inf")
+# for i in range(len(arr)):
+#     if arr[i] < smallest:
+#         smallest = arr[i]
+# print(smallest)        
+
+
+# Q7 — Count Even Numbers
+# arr = [10,25,60,4,50]
+# count = 0
+# for i in range(len(arr)):
+#     if arr[i]%2 == 0:
+#         count = count + 1
+# print(count)        
+
+
+# Find the First Occurrence
+arr = [10,25,60,4,50]
+
+
+
+
--- day3.py
+++ day3.py
@@ -41,18 +41,109 @@
         
         
 # Count Positive, Negative, and Zero
+# n = int(input())
+# positive = 0
+# negative = 0
+# zero = 0
+# for i in range(n):
+#     number = int(input()) 
+#     if number > 0:
+#         positive = positive + 1
+#     elif number < 0:
+#         negative = negative + 1
+#     else:
+#         zero = zero + 1
+# print(positive)
+# print(negative)
+# print(zero)
+
+
+#sum of even numbers
+# n = int(input())
+# sum = 0
+# for i in range(1,n+1):
+#     if i%2 == 0:
+#         sum = sum + i
+# print(sum)
+
+
+#Assessment Q1
+# Write a program that takes an integer n and prints:
+# Positive Even if n is positive and even
+# Positive Odd if n is positive and odd
+# Negative Even if n is negative and even
+# Negative Odd if n is negative and odd
+# Zero if n is zero
+
+
+# n = int(input())
+# if n > 0:
+#     if n % 2 == 0:
+#         print("Positive Even")
+#     else:
+#         print("Positive Odd")
+# elif n < 0:
+#     if n % 2 == 0:
+#         print("Negative Even")
+#     else:
+#         print("Negative Odd")
+# else:
+#     print("Zero")                      
+
+
+
+# Problem
+# Take an integer n as input and calculate the sum of all odd numbers from 1 to n.
+
+# n = int(input())
+# s = 0
+# for i in range(0,n+1):
+#     if i % 2 != 0:
+#         s = s + i
+# print(s)        
+
+
+
+
+# Problem
+# First input an integer n.
+# Then input n numbers.
+# Count how many of those numbers are:
+# Positive
+# Negative
+# Zero
+# Finally print the three counts in that order
+
+
+# n = int(input())
+# p = 0
+# m = 0
+# z = 0
+# for i in range(n):
+#     num = int(input())
+#     if num > 0:
+#         p = p + 1
+#     elif num < 0:
+#         m = m + 1
+#     else:
+#         z = z + 1 
+# print(p)
+# print(m)
+# print(z)                 
+
+
+
+# Problem
+# Input an integer n, then input n numbers.
+# Find and print the largest number among them.
+
 n = int(input())
-positive = 0
-negative = 0
-zero = 0
-for i in range(n):
-    number = int(input()) 
-    if number > 0:
-        positive = positive + 1
-    elif number < 0:
-        negative = negative + 1
-    else:
-        zero = zero + 1
-print(positive)
-print(negative)
-print(zero)
+largest = int(input())
+for i in range(n - 1):
+    num = int(input())
+    if num > largest:
+        largest = num
+print(largest)
+
+
+
